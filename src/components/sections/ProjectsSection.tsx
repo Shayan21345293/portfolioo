399c88bb21082e06cb165279ddf99bb44d262c27
Shayan21345293 <148089130+Shayan21345293@users.noreply.github.com>
2025-07-06 18:18:40 +0500

    linked

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -159,7 +159,7 @@ const ProjectsSection = () => {
               These are just a few highlights. I&apos;m always working on new projects and exploring innovative solutions.
             </p>
             <motion.a
-              href="https://github.com/shayan"
+              href="https://github.com/Shayan21345293"
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ scale: 1.05 }}
